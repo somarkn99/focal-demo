1dc64e4384768787c8435ee70b35956a787f46d6
Somar Kesen <kesen.somar.99@gmail.com>
2024-04-15 10:54:53 +0300

    Item CRUD

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Item;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $items = Item::all();
+        return response()->json($items);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string|max:1000',
+            'quantity' => 'required|integer',
+            'price' => 'required|numeric'
+        ]);
+
+        $item = Item::create($request->all());
+        return response()->json($item, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Item $item)
+    {
+        return response()->json($item);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Item $item)
+    {
+        $request->validate([
+            'name' => 'string|max:255',
+            'description' => 'string|max:1000',
+            'quantity' => 'integer',
+            'price' => 'numeric'
+        ]);
+
+        $item->update($request->all());
+        return response()->json($item);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Item $item)
+    {
+        $item->delete();
+        return response()->json(null, 204); // No content to send back
+    }
+}
---
+++ app/Models/Item.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'price'
+    ];
+
+        /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'quantity' => 'integer',
+        'price' => 'float',
+        'created_at' => 'datetime:Y-m-d',
+        'updated_at' => 'datetime:Y-m-d H:i:s'
+    ];
+}
